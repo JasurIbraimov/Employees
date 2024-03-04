eb4f53de7ad8f13ccfdb1cbb88da2c5ddf8cf6a0
Jasur Ibraimov <ibraimov0409@mail.ru>
2024-03-04 06:18:23 +0500

    Added netlify

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const serverless = require("serverless-http");
 
 require("dotenv").config(); // Setup env
 
@@ -13,4 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/user", require("./routes/users"));
 app.use("/api/employees", require("./routes/employees"));
-module.exports = app;
+
+
+module.exports.handler = serverless(app);
+
